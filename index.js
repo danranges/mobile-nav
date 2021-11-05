3e1024a3f16ee7ad759e549007e22b429eb319e2
Dan Ranges <danranges@gmail.com>
2021-11-05 22:09:28 +0100

    Add nav selection js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// export default function addNavEvents() {}
+function addNavEvents(navButton) {
+  document.addEventListener('click', (e) => {
+    const isNavButton = e.target.matches(`[${navButton}]`);
+
+    if (isNavButton) {
+      const currentNavItem = e.target;
+      document.querySelectorAll(`[${navButton}]`).forEach((navItem) => {
+        if (navItem !== currentNavItem) {
+          navItem.classList.remove('active');
+        }
+      });
+    }
+  });
+}
+
+addNavEvents('data-nav-button');
